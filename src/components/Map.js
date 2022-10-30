a7866e63049b36a4a68c4ed1c53fb74e987ca96e
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-10-30 17:41:37 -0600

    Restructure

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,25 +1,11 @@
 import React from "react";
-import { useLoadScript } from '@react-google-maps/api';
 
-function Map() {
-  const {isLoaded} = useLoadScript({
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-    libraries: ["places"]
-  });
-
-  if (!isLoaded) {
-    return(
-      <React.Fragment>
-        <h1>MAP IS LOADING</h1>
-      </React.Fragment>
-    )
-  }
-
-  return(
+function Map(){
+  return (
     <React.Fragment>
-      <h1>This is a map</h1>
+      <h1>THIS IS A MAP</h1>
     </React.Fragment>
-  );
+    );
 }
 
 export default Map
---
+++ src/components/MapLoader.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Map from "./Map.js"
+import { useLoadScript } from '@react-google-maps/api';
+
+function MapLoader(){
+  const {isLoaded} = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    libraries: ["places"]
+  });
+
+  if (!isLoaded) {
+    return(
+      <React.Fragment>
+        <h1>MAP IS LOADING</h1>
+      </React.Fragment>
+    )
+  }
+
+  return(
+    <React.Fragment>
+      <Map />
+    </React.Fragment>
+  );
+}
+
+export default MapLoader
