79fdcc05813886c9e52a14030d7dbf78d2c0d506
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-11-14 21:32:45 -0700

    Built static station list in Station component

---
+++ src/components/Station.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Station(){
+
+  return (
+    <tbody>
+    <tr>
+      <td>Blue</td>
+      <td>Rosemont</td>
+      <td>O'Hare</td>
+      <td>12:35pm</td>
+    </tr>
+    <tr>
+      <td>Red</td>
+      <td>Jackson</td>
+      <td>Howard</td>
+      <td>1:45pm</td>
+    </tr>
+    <tr>
+      <td>Pink</td>
+      <td>18th</td>
+      <td>54th/Cermak</td>
+      <td>12:55pm</td>
+    </tr>
+    <tr>
+      <td>Pink</td>
+      <td>Morgan</td>
+      <td>Loop 'L'</td>
+      <td>Delayed</td>
+    </tr>
+  </tbody>
+  );
+}
+
+export default Station;
--- src/components/StationList.js
+++ src/components/StationList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Station from './Station';
 // import stops  from './../txt/stops.txt';
 import "./StationList.css";
 
@@ -26,32 +27,7 @@ function StationList(){
                 <th>Arrival Times</th>
               </tr>
             </thead>
-            <tbody>
-              <tr>
-                <td>Blue</td>
-                <td>Rosemont</td>
-                <td>O'Hare</td>
-                <td>12:35pm</td>
-              </tr>
-              <tr>
-                <td>Red</td>
-                <td>Jackson</td>
-                <td>Howard</td>
-                <td>1:45pm</td>
-              </tr>
-              <tr>
-                <td>Pink</td>
-                <td>18th</td>
-                <td>54th/Cermak</td>
-                <td>12:55pm</td>
-              </tr>
-              <tr>
-                <td>Pink</td>
-                <td>Morgan</td>
-                <td>Loop 'L'</td>
-                <td>Delayed</td>
-              </tr>
-            </tbody>
+            <Station />
           </table>
         </div>
       </div>
