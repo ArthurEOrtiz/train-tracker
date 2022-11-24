140738377a5a6dc87b1288da87e4092611af23ba
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-11-24 13:58:02 -0700

    Update StationMarker.js
    will return stations  with lat and long

--- src/components/StationMarker.js
+++ src/components/StationMarker.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect} from 'react';
+import { startTransition } from 'react';
 // import { Marker } from "@react-google-maps/api";
 
 function StationMarker() {
@@ -22,15 +23,15 @@ function StationMarker() {
       setIsLoaded(true)
       return jsonResponse
     })
-    .then((data) => {
-        setStations(
-          data.reduce((stations, stops, i) =>{
-              stations[stops.station_name] = 
-                  data.filter(s => (s.map_id.includes(stops.map_id)));
-                  return stations;
-                },{})
-        )
-    })
+    // .then((data) => {
+    //     setStations(
+    //       data.reduce((stations, stops, i) =>{
+    //           stations[stops.station_name] = 
+    //               data.filter(s => (s.map_id.includes(stops.map_id)));
+    //               return stations;
+    //             },{})
+    //     )
+    // })
     .catch((error) => {
       setError(error)
       setIsLoaded(true)
@@ -38,8 +39,26 @@ function StationMarker() {
 
   },[])
 
-  // console.log(stopData)
-  console.log(Stations)
+  const stations = stopData.reduce((stations, stop, i)=>{
+    let stopsInStation = []
+
+    stations[stop.station_name] = {
+      map_id: stop.map_id,
+      lat: +stop.location.latitude, 
+      lng: +stop.location.longitude,
+      // lines: {
+      //   blue: ((stop.blue)? true : "")
+      // }
+      // lines: [((stop.pnk)? "Pink" : null), [(stop.blue)? "Blue": null]],
+      // stops: stopData.filter(s => s.map_id.includes(stop.map_id))
+      stops : stopsInStation.push("1")
+    }
+
+      return stations;
+  },{});
+
+  console.log(stations);
+  // console.log(stopData[0].blue);
 
   if (error) {
     return (
