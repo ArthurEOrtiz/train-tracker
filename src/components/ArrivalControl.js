5ddac0065f2b0f8c91f551966074d4c8e6af7749
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-12-10 13:00:19 -0700

    Update ArrivalControl.js
    Able to GET data from CTA API and concat into an array of objects

--- src/components/ArrivalControl.js
+++ src/components/ArrivalControl.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 // import { useCallback } from 'react';
 import ArrivalList from './ArrivalList';
 import Map from './Map';
@@ -76,23 +76,41 @@ function ArrivalControl(){
     },[]);
 
 
-  const fetchCTAArrivals = mapIdsToDisplay.reduce((acc, element)=>{
-    const data = fetch(`http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=${process.env.REACT_APP_CTA_API_KEY}&mapid=${element}&outputType=JSON`)
-    .then(response=> {
-      return response.json()
-    })
+  // const fetchCTAArrivals = mapIdsToDisplay.reduce((acc, element)=>{
+  //   const data = fetch(`http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=${process.env.REACT_APP_CTA_API_KEY}&mapid=${element}&outputType=JSON`)
+  //   .then(response=> {
+  //     return response.json()
+  //   })
 
-    return acc.concat(data);
-  },[])
+  //   return acc.concat(data);
+  // },[])
 
 
   // console.log(Object.keys(fetchCTAArrivals));
-  console.log(fetchCTAArrivals);
-  // console.log(mapIdsToDisplay);
-  // console.log(arrivals);
+  // console.log(fetchCTAArrivals);
   
 
+  
+  const getArrivals = async (mpid) => {
 
+    const res = await fetch(`http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=${process.env.REACT_APP_CTA_API_KEY}&mapid=${mpid}&outputType=JSON`);
+
+    const data = await res.json();
+    
+    setArrivals(arrivals.concat(data))
+
+  };
+
+  useEffect(()=>{
+    mapIdsToDisplay.forEach(s => getArrivals(s));
+  },[selectedStations]);
+
+  // mapIdsToDisplay.forEach(s => getArrivals(s));
+
+  console.log(arrivals);
+  // console.log(mapIdsToDisplay);
+
+  
 
   if (error) {
     return (
