8d6c9898b7bed1f3e208adc8c9c82104815e45f0
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2023-03-01 20:34:15 -0700

    Update ArrivalControl.js
    Use async/await instead of .then chaining.

--- src/components/ArrivalControl.js
+++ src/components/ArrivalControl.js
@@ -14,54 +14,52 @@ function ArrivalControl(){
   
 
    // Station API
-    useMemo(()=>{
-    fetch(`https://data.cityofchicago.org/resource/8pix-ypme.json?`)
-    .then(response => {
-      console.log(`API has been called: ${response.ok}`)
-      if(!response.ok) {
-        throw new Error(`${response.status}: ${response.statusText}`);
-      } else {
-        return response.json();
-      }
-    })
-    .then((jsonResponse) => {
-        setStations(
-          jsonResponse.reduce((stations, stop, i, array )=>{
-            const stopsArray = array.filter(s => s.map_id === stop.map_id);
-            const stopsObj = stopsArray.map(s => {
-              return {  stop_id: s.stop_id,
-                        stop_name: s.stop_name,
-                        ada: s.ada,
-                        red: s.red,
-                        blue: s.blue,
-                        green: s.g,
-                        brown: s.brn,
-                        purple: s.p,
-                        purple_express: s.pexp,
-                        yello: s.y,
-                        pink: s.pnk,
-                        orange: s.o
-                      }
-              }
-            );
-            stations[stop.map_id] = {
-                      station_name: stop.station_name,
-                      map_id: +stop.map_id,
-                      lat: +stop.location.latitude, 
-                      lng: +stop.location.longitude,
-                      stops : stopsObj
-                    }
-                      return stations;
-          },[])
-        )
 
-        setIsLoaded(true)
-    })
-    .catch((error) => {
-      setError(error.message);
-      setIsLoaded(true);
-    });
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`https://data.cityofchicago.org/resource/8pix-ypme.json?`);
+        if (!response.ok) {
+          throw new Error(`${response.status}: ${response.statusText}`);
+        }
+        const jsonResponse = await response.json();
+        const stationsData = jsonResponse.reduce((stations, stop, i, array ) => {
+          const stopsArray = array.filter(s => s.map_id === stop.map_id);
+          const stopsObj = stopsArray.map(s => {
+            return {
+              stop_id: s.stop_id,
+              stop_name: s.stop_name,
+              ada: s.ada,
+              red: s.red,
+              blue: s.blue,
+              green: s.g,
+              brown: s.brn,
+              purple: s.p,
+              purple_express: s.pexp,
+              yello: s.y,
+              pink: s.pnk,
+              orange: s.o
+            }
+          });
+          stations[stop.map_id] = {
+            station_name: stop.station_name,
+            map_id: +stop.map_id,
+            lat: +stop.location.latitude,
+            lng: +stop.location.longitude,
+            stops : stopsObj
+          }
+          return stations;
+        },[]);
+        setStations(stationsData);
+        setIsLoaded(true);
+      } catch (error) {
+        setError(error.message);
+        setIsLoaded(true);
+      }
+    };
+    fetchData();
   }, []);
+  
 
   const handleStationSelection = (id) => {
 
